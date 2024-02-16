77cb80f838649e3bfb76887a43d2c649e3164256
Alyssa Paynter <114109419+alyspayn@users.noreply.github.com>
2024-02-16 13:02:43 -0600

    City-Block Dst, Multiple ROC, Adjust Plots

--- singleloc_analysis.py
+++ singleloc_analysis.py
@@ -1,10 +1,9 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.metrics import confusion_matrix, classification_report, brier_score_loss, roc_auc_score, RocCurveDisplay
-from sklearn.calibration import CalibratedClassifierCV, CalibrationDisplay, calibration_curve
+from sklearn.metrics import confusion_matrix, classification_report, brier_score_loss, roc_auc_score, RocCurveDisplay, roc_curve
 import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
+import sys
 import pyard
 ard = pyard.init("3520")
 
@@ -58,7 +57,7 @@ def neg_prediction(truth_typ1,truth_typ2,impute_typ1,impute_typ2):
     #     neg_count += 1
     # if (neg_count == 2) & (donor_homoz == 1):
     #     neg_count = 1
-    # # Want to make it to where 1=correct prediction and -0=incorrect prediction
+    # # Want to make it to where 1=correct prediction and 0=incorrect prediction
     # if neg_count >= 1:
     #     neg_count = 0
     # else:
@@ -66,8 +65,9 @@ def neg_prediction(truth_typ1,truth_typ2,impute_typ1,impute_typ2):
 
     return neg_count
 
-truth_filename = 'genotype_truth_table.csv'
-impute_filename = 'lowres_topprob_impute.csv'
+
+truth_filename = 'genotype_truth_table.csv'  # sys.argv[1]
+impute_filename = 'lowres_topprob_impute.csv'   # sys.argv[2]
 truth_table = pd.read_csv(truth_filename, header=0)
 impute = pd.read_csv(impute_filename, header=0)
 
@@ -132,15 +132,15 @@ brier_loss = {}
 roc_auc = {}
 for locus in loci:
     # Need everything to be in numpy array for sklearn metrics
-    probs = impute[locus + '_Prob'].to_numpy()                  # Probability of correctness in terms of -1/1
+    probs = impute[locus + '_Prob'].to_numpy()                  # Probability of correctness in terms of 0/1
     probability = impute['GENO_' + locus + '_Prob'].to_numpy()  # Probability of correctness in [0,1] terms
-    true_pred = impute[locus + '_True'].to_numpy()              # Actual prediction in terms of -1/1
+    true_pred = impute[locus + '_True'].to_numpy()              # Actual prediction in terms of 0/1
 
     # Confusion Matrix per Locus
-    print('Confusion Matrix for Locus: ', locus)
-    print(confusion_matrix(true_pred, probs))
+    print('Classification Report for Locus ', locus)
+    # print(confusion_matrix(true_pred, probs))
     tn, fp, fn, tp = confusion_matrix(true_pred, probs).ravel()
-    print('TP: ' + str(tp) + ', TN: ' + str(tn) + ', FP: ' + str(fp) + ', FN: ' + str(fn))
+    # print('TP: ' + str(tp) + ', TN: ' + str(tn) + ', FP: ' + str(fp) + ', FN: ' + str(fn))
 
     confusion_mat_line = pd.DataFrame({'TN': tn, 'FP': fp, 'FN': fn, 'TP': tp}, index=[locus])
     confusion_mat = pd.concat([confusion_mat, confusion_mat_line])
@@ -150,11 +150,11 @@ for locus in loci:
 
     # Brier Score Loss
     brier_loss[locus] = brier_score_loss(true_pred, probability > threshold)
-    print('Brier Score Loss: ', brier_loss[locus])
+    # print('Brier Score Loss: ', brier_loss[locus])
 
     # ROC-AUC Score
     roc_auc[locus] = roc_auc_score(true_pred, probability)
-    print('ROC-AUC Score: ', roc_auc[locus])
+    # print('ROC-AUC Score: ', roc_auc[locus])
 
     # Sort the Dataset based on probabilities to allow the low/high probabilities to be together in the split
     impute_sort = impute.sort_values(by=['GENO_' + locus + '_Prob'])
@@ -180,13 +180,17 @@ for locus in loci:
     snd_err4 = np.std(prob_q4, ddof=1) / np.sqrt(np.size(prob_q4))
     snd_err = [snd_err1, snd_err2, snd_err3, snd_err4]
 
+    # Compute the city-block distance between the quantiles and the diagonal (x=y)
+    city_block_dst = (abs(probability_avg[0] - true_avg[0]) + abs(probability_avg[1] - true_avg[1]) + abs(probability_avg[2] - true_avg[2]) + abs(probability_avg[3] - true_avg[3])) / n_bins
+
+
     # Create a table exactly like the print statements above to add to the bottom of the calibration plot
-    table_data = [["Quartile", "Prob Avg", "True Fraction", "Min Prob", "Max Prob", "Standard Error"],
+    table_data = [["Quantile", "Prob Avg", "True Fraction", "Min Prob", "Max Prob", "Standard Error"],
                   ['Q1', str(round(probability_avg[0], 4)), str(round(true_avg[0], 4)), str(round(min_prob_in_bin[0], 4)), str(round(max_prob_in_bin[0], 4)), str(round(snd_err1, 4))],
                   ['Q2', str(round(probability_avg[1], 4)), str(round(true_avg[1], 4)), str(round(min_prob_in_bin[1], 4)), str(round(max_prob_in_bin[1], 4)), str(round(snd_err2, 4))],
                   ['Q3', str(round(probability_avg[2], 4)), str(round(true_avg[2], 4)), str(round(min_prob_in_bin[2], 4)), str(round(max_prob_in_bin[2], 4)), str(round(snd_err3, 4))],
                   ['Q4', str(round(probability_avg[3], 4)), str(round(true_avg[3], 4)), str(round(min_prob_in_bin[3], 4)), str(round(max_prob_in_bin[3], 4)), str(round(snd_err4, 4))]]
-
+    print('Quantile Statistics for Locus: ', locus)
     print(table_data[0])
     print(table_data[1])
     print(table_data[2])
@@ -201,29 +205,46 @@ for locus in loci:
     calibrat_plot = plt.figure(figsize=(8, 8))
     calibrat_plot = plt.errorbar(probability_avg, true_avg, yerr=snd_err, marker='o', linestyle='', label='True Fraction vs Probability Average for Quartile', color='red', ecolor='black', capsize=7)
     calibrat_plot = plt.plot([0,1], linestyle='--', label='Ideal Calibration', color='blue')
-    # calibrat_plot = plt.xlabel('Mean Predicted Probability within Quartile for ' + locus)
+    calibrat_plot = plt.xlabel('Mean Predicted Probability for Quantile')
     calibrat_plot = plt.ylabel('Fraction of Predictions Correct')
     # plt.yscale('log')
     # plt.xscale('log')
-    calibrat_plot = plt.title('Calibration Plot and Prediction Probability Distribution for HLA-' + locus + " Locus\n" + 'Brier Score Loss: ' + str(round(brier_loss[locus], 4)))
+    # calibrat_plot = plt.suptitle('Calibration Plot and Prediction Probability Distribution for HLA-' + locus + " Locus\n" + 'Brier Score Loss: ' + str(round(brier_loss[locus], 4)) + '\n                        \n')
     calibrat_plot = plt.bar(bins[:-1], fract_counts, width=np.diff(bins), edgecolor='black', color='grey')
     calibrat_plot = plt.xlim(0,1.05)
     calibrat_plot = plt.ylim(0,1.05)
     table = plt.table(cellText=table_data)
     table = table.scale(1, 1.5)
     ax = plt.gca()
-    ax.get_xaxis().set_visible(False)
+    ax.xaxis.set_label_position('top')  # have to add x-axis to the top because of the table at the bottom
+    ax.xaxis.set_ticks_position('top')
+    ax.set_title('Calibration Plot and Prediction Probability Distribution for HLA-' + locus + " Locus\n" + 'Brier Score Loss: ' + str(round(brier_loss[locus], 4)) + '\n' + 'City-Block Distance: ' + str(round(city_block_dst, 4)), pad=20)  # Space between x-axis and title
     calibrat_plot = plt.legend()
     calibrat_plot = plt.savefig("Calibration_" + locus + ".png", bbox_inches='tight')
     # plt.show()
 
+plt.figure(0)
+for locus in loci:
     # Display ROC plots
-    # TODO - create a plot with multiple ROCs on one plot
-    roc_plot = RocCurveDisplay.from_predictions(true_pred, probability, plot_chance_level=True)
-    roc_plot = plt.title('ROC Curve and AUC for ' + locus)
-    roc_plot = plt.savefig("ROC_" + locus + ".png", bbox_inches='tight')
-    # plt.show()
-
+    probability = impute['GENO_' + locus + '_Prob'].to_numpy()  # Probability of correctness in [0,1] terms
+    true_pred = impute[locus + '_True'].to_numpy()  # Actual prediction in terms of 0/1
+    # Shows the old code for how to do one ROC per locus
+    # roc_plot = RocCurveDisplay.from_predictions(true_pred, probability, plot_chance_level=True)
+    # roc_plot = plt.title('ROC Curve and AUC for ' + locus)
+    # roc_plot = plt.savefig("ROC_" + locus + ".png", bbox_inches='tight')
+    # # plt.show()
+    fpr, tpr, thresholds = roc_curve(true_pred, probability)
+    # Calculate Area under the curve to display on the plot
+    auc = roc_auc_score(true_pred, probability)
+    # Now, plot the computed values
+    roc_plot = plt.plot(fpr, tpr, label='%s (AUC = %0.2f)' % (locus, auc))
+    # Custom settings for the plot
+roc_plot = plt.plot([0, 1], 'r--', color='black')
+roc_plot = plt.xlabel('Specificity (False Positive Rate)')
+roc_plot = plt.ylabel('Sensitivity (True Positive Rate)')
+roc_plot = plt.title('ROC-AUC Curves for each Locus Predictor')
+roc_plot = plt.legend(loc="lower right")
+roc_plot = plt.savefig("ROC_AUC_9loc.png", bbox_inches='tight')
 
 bf = pd.DataFrame({'Brier_Loss_Score': brier_loss}, index=brier_loss.keys())
 ra = pd.DataFrame({'ROC-AUC': roc_auc}, index=roc_auc.keys())
