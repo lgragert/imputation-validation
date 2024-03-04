0b685b705b93d9b6dc5d85c5007ebfe5ce2f6371
Alyssa Paynter <114109419+alyspayn@users.noreply.github.com>
2024-03-04 08:49:48 -0600

    Takes arguments for DRDQ, DR, and DQ

--- montecarlo_pairings.py
+++ montecarlo_pairings.py
@@ -15,14 +15,16 @@ def clean_eplet_str(eplet_list_df):
 
 
 # Select random pairings from the truth table
-truth_filename = 'DRDQ_pairs_truth.csv'
+which_impute = 'DRDQ'
+truth_filename = which_impute + '_pairs_truth.csv'
 truth_pairs = pd.read_csv(truth_filename, header=0)
 
 # Start with 100 random pairs as that is what the API can handle: 100, 200, 500, 1,000... until API breaks
 n_pairs = 100
 n_truthpairs = truth_pairs.sample(n=n_pairs, random_state=2272024).reset_index(drop=True)  # Initialize state, to keep it consistent
+print(n_truthpairs)
 
-n_truth_file = 'DRDQ_pairs_truth_' + str(n_pairs) + ".csv"
+n_truth_file = which_impute + '_pairs_truth_' + str(n_pairs) + ".csv"
 n_truthpairs.to_csv(n_truth_file, header=True, index=False)
 
 # Create a dictionary of pairings to find them in the imputation file
@@ -71,16 +73,22 @@ for line in range(len(n_truthpairs)):
                 eplet_df[key + "_" + 'details'] = clean_eplet_str(eplet_df[key + "_" + 'details'])
             e_df = pd.concat([e_df, eplet_df], axis=1)
     eplet_truth = pd.concat([eplet_truth, e_df])
+    print(eplet_truth)
     time.sleep(1.0)
 
 # Clean the format a little bit to only the columns you want
-eplet_truth_table = eplet_truth[['ALL_quantity', 'ALL_details', 'DRB_quantity', 'DRB_details', 'DQ_quantity', 'DQ_details']].reset_index(names=['ID'])
+if which_impute == 'DRDQ':
+    eplet_truth_table = eplet_truth[['ALL_quantity', 'ALL_details']].reset_index(names=['ID'])
+elif which_impute == 'DR':
+    eplet_truth_table = eplet_truth[['DRB_quantity', 'DRB_details']].reset_index(names=['ID'])
+elif which_impute == 'DQ':
+    eplet_truth_table = eplet_truth[['DQ_quantity', 'DQ_details']].reset_index(names=['ID'])
 print(eplet_truth_table)
-eplet_truth_table.to_csv('eplet_truth_table' + str(n_pairs) + '.csv', index=False, header=True)
+eplet_truth_table.to_csv(which_impute + '_eplet_truth_table' + str(n_pairs) + '.csv', index=False, header=True)
 
 
 # Run through all possibilities for those pairings in the imputation file
-impute_file = 'DRDQ_pairs_imputation.csv'
+impute_file = which_impute + '_pairs_imputation.csv'
 impute_pairs = pd.read_csv(impute_file, header=0)
 
 n_impute_pairs = pd.DataFrame()
@@ -91,16 +99,15 @@ for pair in rand_pairs:
     if line.empty is True:
         line = impute_pairs[(impute_pairs['DON_ID'] == rec) & (impute_pairs['REC_ID'] == don)]
 
-    line = line.sort_values(by=['PairProb'], ascending=False)
-    line['Rank'] = range(1, len(line) + 1)
+    line = line.sort_values(by=['PairProb_' + which_impute], ascending=False)
 
-    prob_sum = line['PairProb'].sum()
+    prob_sum = line['PairProb_' + which_impute].sum()
     print(pair + ": " + str(prob_sum))  # Check each pair's probability == 1
 
     n_impute_pairs = pd.concat([n_impute_pairs, line])
 
 n_impute_pairs = n_impute_pairs.reset_index(drop=True)
-n_impute_filename = 'DRDQ_pairs_imputation_' + str(n_pairs) + ".csv"
+n_impute_filename = which_impute + '_pairs_imputation_' + str(n_pairs) + ".csv"
 n_impute_pairs.to_csv(n_impute_filename, header=True, index=False)
 
 # Put the imputation through the eplet API
@@ -108,9 +115,9 @@ eplet_dataframe = pd.DataFrame()
 for line in range(len(n_impute_pairs)):
     donor_ID = n_impute_pairs.loc[line, 'DON_ID']
     recip_ID = n_impute_pairs.loc[line, 'REC_ID']
-    donor_geno_DRDQ = n_impute_pairs.loc[line, 'DON_GLString']
-    recip_geno_DRDQ = n_impute_pairs.loc[line, 'REC_GLString']
-    pair_prob = n_impute_pairs.loc[line, 'PairProb']
+    donor_geno_DRDQ = n_impute_pairs.loc[line, 'DON_' + which_impute]
+    recip_geno_DRDQ = n_impute_pairs.loc[line, 'REC_' + which_impute]
+    pair_prob = n_impute_pairs.loc[line, 'PairProb_' + which_impute]
     id_pair = donor_ID + "+" + recip_ID
 
     # Can only handle a 100 pairs at a time, so make sure to take a break
@@ -118,7 +125,7 @@ for line in range(len(n_impute_pairs)):
         time.sleep(60.0)
 
     # Need to know which genotypes are used for each donor-recipient pairing corresponding to the IDs
-    donor_line = pd.DataFrame({'DON_ID': donor_ID, 'DON_GLString': donor_geno_DRDQ, 'REC_ID': recip_ID, 'REC_GLString': recip_geno_DRDQ, "PairProb": pair_prob}, index=[id_pair])
+    donor_line = pd.DataFrame({'DON_ID': donor_ID, 'DON_' + which_impute: donor_geno_DRDQ, 'REC_ID': recip_ID, 'REC_' + which_impute: recip_geno_DRDQ, "PairProb_" + which_impute: pair_prob}, index=[id_pair])
 
     immunizer_alleles = donor_geno_DRDQ
     patient_alleles = recip_geno_DRDQ
@@ -145,6 +152,12 @@ for line in range(len(n_impute_pairs)):
     print(eplet_dataframe)
     time.sleep(1.0)
 
-eplet_impute = eplet_dataframe[['DON_ID', 'REC_ID', 'PairProb', 'DON_GLString', 'REC_GLString', 'ALL_quantity', 'ALL_details', 'DRB_quantity', 'DRB_details', 'DQ_quantity', 'DQ_details']].reset_index(drop=True)  # Only need these columns for now
+if which_impute == 'DRDQ':
+    eplet_impute = eplet_dataframe[['DON_ID', 'REC_ID', 'PairProb_' + which_impute, 'DON_' + which_impute, 'REC_' + which_impute, 'ALL_quantity', 'ALL_details']].reset_index(drop=True)  # Only need these columns for now
+elif which_impute == 'DR':
+    eplet_impute = eplet_dataframe[['DON_ID', 'REC_ID', 'PairProb_' + which_impute, 'DON_' + which_impute, 'REC_' + which_impute, 'DRB_quantity', 'DRB_details']].reset_index(drop=True)
+elif which_impute == 'DQ':
+    eplet_impute = eplet_dataframe[['DON_ID', 'REC_ID', 'PairProb_' + which_impute, 'DON_' + which_impute, 'REC_' + which_impute, 'DQ_quantity', 'DQ_details']].reset_index(drop=True)  # Only need these columns for now
+
 print(eplet_impute)
-eplet_impute.to_csv('eplet_lowres_impute' + str(n_pairs) + '.csv', header=True, index=False)
+eplet_impute.to_csv(which_impute + '_eplet_lowres_impute' + str(n_pairs) + '.csv', header=True, index=False)
