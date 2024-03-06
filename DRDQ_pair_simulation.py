91b12ec1257dfa07be8bd273fe007adf1e52eaa0
Alyssa Paynter <114109419+alyspayn@users.noreply.github.com>
2024-03-06 09:30:06 -0600

    Uses itertools combinations()

--- DRDQ_pair_simulation.py
+++ DRDQ_pair_simulation.py
@@ -2,6 +2,8 @@
 import pandas as pd
 from collections import defaultdict
 import math
+from itertools import combinations
+import sys
 # Create pairings for the dataset of 216 subjects.
 
 
@@ -38,10 +40,10 @@ def geno_pairs(GF_dict, don_geno, rec_geno, donID, recID, pair_ids, whichimpute)
 
 
 # Clean truth table to only have DR-DQ genotypes and the simulated donor-recipient pairs
-which_impute = sys.argv[1]  # 'DRDQ'   # will change this parameter from DRDQ, DR, or DQ
-truth_file = sys.argv[2]  # 'genotype_truth_table.csv'
+which_impute = 'DRDQ'   # will change this parameter from DRDQ, DR, or DQ
+truth_file = 'genotype_truth_table.csv'
 truth_table = pd.read_csv(truth_file, header=0)
-impute_file = sys.argv[3]  # 'lowres_' + which_impute + '_impute.csv'
+impute_file = 'lowres_' + which_impute + '_impute.csv'
 impute_lowres = pd.read_csv(impute_file, header=0)
 truth_table = truth_table[truth_table.ID.isin(impute_lowres.ID)].reset_index(drop=True)
 truth_table = sep_glstring(truth_table)
@@ -73,45 +75,36 @@ print("Number of Unique Genotypes in Truth Table: ", len(TT_DRDQ))
 print("Number of Pairings for Truth Table Genotypes: ", math.comb(len(TT_DRDQ), 2))
 
 # Create truth dictionary with {ID: genotype: 1}
-TT_DRDQ = defaultdict(dict)
+TT_DRDQ = {}
 for line in range(len(truth_table)):
     subject_id = truth_table.loc[line, 'ID']
     GENO_DRDQ = truth_table.loc[line, which_impute + '_GLString']
 
     if GENO_DRDQ not in TT_DRDQ:
-        TT_DRDQ[subject_id][GENO_DRDQ] = 1
+        TT_DRDQ[subject_id] = GENO_DRDQ
     else:
         continue
 
 print("Number of IDs in Truth Table: ", len(TT_DRDQ))
+truth_id = truth_table['ID'].to_list()
 print("Number of Pairings for Truth Table IDs: ", math.comb(len(TT_DRDQ), 2))
 
 # Simulate highres "donor-recipient" pairs from the truth dataset by looping through all combinations
-truth_pairs = {}
 eplet_truth = pd.DataFrame()
-for donor_ID in TT_DRDQ:
-    for recip_ID in TT_DRDQ:
-        if donor_ID == recip_ID:  # skip pairs with same ID
-            continue
+for don, rec in combinations(truth_id, 2):
+    pair_id = "+".join([don, rec])
+    don_geno = TT_DRDQ.get(don)
+    rec_geno = TT_DRDQ.get(rec)
 
-        # skip pairs where ID was already computed
-        # (donor_ID, recip_ID) = sorted([donor_ID, recip_ID])
-        id_pair = "+".join([donor_ID, recip_ID])
-        if "+".join([recip_ID, donor_ID]) not in truth_pairs:
-            truth_pairs[id_pair] = 1
+    # Clean up the strings to API format
+    immunizer_alleles = don_geno
+    immunizer_alleles = immunizer_alleles.replace('^', ',').replace('+', ',')
+    patient_alleles = rec_geno
+    patient_alleles = patient_alleles.replace('^', ',').replace('+', ',')
 
-            # loop through all possible genotypes for donor and recip pairing
-            for donor_geno_DRDQ in TT_DRDQ[donor_ID]:
-                for recip_geno_DRDQ in TT_DRDQ[recip_ID]:
-                    immunizer_DRDQ = donor_geno_DRDQ
-                    immunizer_DRDQ = immunizer_DRDQ.replace('^', ',').replace('+', ',')
-                    patient_DRDQ = recip_geno_DRDQ
-                    patient_DRDQ = patient_DRDQ.replace('^', ',').replace('+', ',')
+    donrec_line = pd.DataFrame({'DON_ID': don, 'DON_GLString': immunizer_alleles, 'REC_ID': rec, 'REC_GLString': patient_alleles}, index=[pair_id])
+    eplet_truth = pd.concat([eplet_truth, donrec_line])
 
-                    donrec_line = pd.DataFrame(
-                        {'DON_ID': donor_ID, 'DON_GLString': immunizer_DRDQ, 'REC_ID': recip_ID,
-                         'REC_GLString': patient_DRDQ}, index=[id_pair])
-                    eplet_truth = pd.concat([eplet_truth, donrec_line])
 
 # Clean the format a little bit to only the columns you want
 eplet_truth_table = eplet_truth.sort_index().reset_index(drop=True)
@@ -121,8 +114,6 @@ eplet_truth_table.to_csv(which_impute + '_pairs_truth.csv', header=True, index=F
 # Create file for multiple imputations
 # Create a dictionary of the dataset such that GF_DRDQ[GENO_DRDQ][subject_id]
 GF_DRDQ = defaultdict(list)
-GF_DR = defaultdict(list)
-GF_DQ = defaultdict(list)
 for line in range(len(impute_lowres)):
     subject_id = impute_lowres.loc[line, 'ID']
     GENO_DRDQ = impute_lowres.loc[line, which_impute + '_GLString']
@@ -136,8 +127,6 @@ print("Number of Pairings for Multiple Imputation Genotypes: ", math.comb(len(GF
 
 # Create a dictionary of the dataset such that GF_DRDQ[subject_ID][GENO_DRDQ][DRDQ_freq]
 GF_DRDQ = defaultdict(dict)
-GF_DR = defaultdict(dict)
-GF_DQ = defaultdict(dict)
 for line in range(len(impute_lowres)):
     subject_id = impute_lowres.loc[line, 'ID']
     GENO_DRDQ = impute_lowres.loc[line, which_impute + '_GLString']
@@ -150,27 +139,14 @@ print("Number of lines in Multiple Imputation: ", len(impute_lowres))
 print("Number of Pairings for Multiple Imputations lines: ", math.comb(len(impute_lowres), 2))
 
 # Use imputation output, compute eplet MM for each possible donor and recipient and store it into a DataFrame
-pair_computed = {}
 eplet_DRDQ = pd.DataFrame()
-eplet_DR = pd.DataFrame()
-eplet_DQ = pd.DataFrame()
-for donor_ID in GF_DRDQ:
-    for recip_ID in GF_DRDQ:
-        if donor_ID == recip_ID:  # skip pairs with same ID
-            continue
+for don, rec in combinations(truth_id, 2):
+    pair_id = "+".join([don, rec])
 
-        # skip pairs where ID was already computed
-        # (donor_ID, recip_ID) = sorted([donor_ID, recip_ID])
-        id_pair = "+".join([donor_ID, recip_ID])
-        if "+".join([recip_ID, donor_ID]) not in pair_computed:
-            pair_computed[id_pair] = 1
-
-            # loop through all possible genotypes for donor and recip pairing - should have made this a definition
-            for donor_geno_DRDQ in GF_DRDQ[donor_ID]:
-                for recip_geno_DRDQ in GF_DRDQ[recip_ID]:
-                    donrec = geno_pairs(GF_DRDQ, donor_geno_DRDQ, recip_geno_DRDQ, donor_ID, recip_ID, id_pair, which_impute)
-                    eplet_DRDQ = pd.concat([eplet_DRDQ, donrec])
-
+    for donor_geno_DRDQ in GF_DRDQ[don]:
+        for recip_geno_DRDQ in GF_DRDQ[rec]:
+            donrec = geno_pairs(GF_DRDQ, donor_geno_DRDQ, recip_geno_DRDQ, don, rec, pair_id, which_impute)
+            eplet_DRDQ = pd.concat([eplet_DRDQ, donrec])
 
 eplet_impute = eplet_DRDQ.sort_index().reset_index(drop=True)  # Only need these columns for now
 print(eplet_impute)
